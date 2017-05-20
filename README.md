70e1bc7f579b3be6c72f238a7020bb64952b16c3
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2017-05-20 16:32:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# sp-build-tasks
+
+[![NPM](https://nodei.co/npm/sp-build-tasks.png?mini=true&downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/sp-build-tasks/)
+
+[![npm version](https://badge.fury.io/js/sp-build-tasks.svg)](https://badge.fury.io/js/sp-build-tasks)
+[![Downloads](https://img.shields.io/npm/dm/sp-build-tasks.svg)](https://www.npmjs.com/package/sp-build-tasks)
+
+
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const del = require('del');
+const tsc = require('gulp-typescript');
+
+const tsconfig = require('./tsconfig.json');
+
+// Clean dist folder
+gulp.task('clean', () => {
+    return del(['dist/**']);
+});
+
+// Compile TypeScript files
+gulp.task('tsc', () => {
+    return gulp
+        .src(['./src/**/*.ts'])
+        .pipe(tsc.createProject('tsconfig.json')())
+        .js.pipe(gulp.dest('./dist'));
+});
