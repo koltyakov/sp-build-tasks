ec4418ace508a19a0643bac76b820de660b24795
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2018-08-03 15:12:40 +0300

    Analyse with wrong paths fix

--- src/tasks/analyze.ts
+++ src/tasks/analyze.ts
@@ -1,5 +1,6 @@
 import { Gulp } from 'gulp';
-import { join } from 'path';
+import { join, resolve } from 'path';
+import { existsSync } from 'fs';
 
 import { getConfigs } from './config';
 import { execPromise } from '../utils/misc';
@@ -21,13 +22,15 @@ export const analyzeTasks = (gulp: Gulp, $: any, settings: ISPBuildSettings) => 
         target: 'app.js'
       }];
       for (const item of webpackItemsMaps) {
-        const target = `${configs.appConfig.distFolder}/scripts/${item.target}`;
+        const target = resolve(`${configs.appConfig.distFolder}/scripts/${item.target}`);
         let sourceMap = extractSourcemapPath(target);
         if (sourceMap) {
-          sourceMap = `${configs.appConfig.distFolder}/scripts/${sourceMap.split('?')[0]}`;
-          const res = await execPromise(`${join(process.cwd(), 'node_modules/.bin', 'source-map-explorer')} ${target} ${sourceMap}`);
-          res.stdout && console.log(`${res.stdout}`);
-          res.stderr && console.log(`${res.stderr}`);
+          sourceMap = resolve(`${configs.appConfig.distFolder}/scripts/${sourceMap.split('?')[0]}`);
+          if (existsSync(sourceMap)) {
+            const res = await execPromise(`${join(process.cwd(), 'node_modules/.bin', 'source-map-explorer')} ${target} ${sourceMap}`);
+            res.stdout && console.log(`${res.stdout}`);
+            res.stderr && console.log(`${res.stderr}`);
+          }
         } else {
           console.log(`No source map found in ${target}`);
         }
