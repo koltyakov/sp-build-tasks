acd36c2823fcfd23cb82102e123a388678d05749
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-04-15 09:08:28 +0300

    Serve mode only errors stats

--- src/tasks/serve.ts
+++ src/tasks/serve.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 import { Gulp } from 'gulp';
 import * as webpack from 'webpack';
 import * as WebpackDevServer from 'webpack-dev-server';
+import * as minimist from 'minimist';
 
 import { processStepMessage } from '../utils/log';
 import { getConfigs } from './config';
@@ -33,6 +34,9 @@ export const serveTasks = (gulp: Gulp, $: any, settings: ISPBuildSettings) => {
 };
 
 const startWebpackDevServer = () => {
+
+  const args = minimist(process.argv.slice(3));
+
   let webpackConfigPath: string = path.join(process.cwd(), 'webpack.config.js');
   if (!fs.existsSync(webpackConfigPath)) {
     webpackConfigPath = path.join(__dirname, '../webpack/config.js');
@@ -43,8 +47,19 @@ const startWebpackDevServer = () => {
   }
   const options = webpackConfig[webpackConfig.length - 1].devServer;
   const port = options.port || 9090;
+  if (typeof options.stats === 'undefined') {
+    options.stats = 'errors-only';
+  }
+
+  if (webpackConfig.length > 4) {
+    console.log(
+      `Warning: It's too name entry points in your Webpack configuration!\n` +
+      `         Use "--webpack-targets=target1.js,target2.js" parameter to reduce a number of entry/targets.`
+    );
+  }
 
   const server = new WebpackDevServer(webpack(webpackConfig), options);
+
   server.listen(port, 'localhost', err => {
     if (err) {
       console.log(err);
@@ -52,4 +67,5 @@ const startWebpackDevServer = () => {
     }
     console.log(`Webpack Dev Server has been starter: http://localhost:${port}`);
   });
+
 };
