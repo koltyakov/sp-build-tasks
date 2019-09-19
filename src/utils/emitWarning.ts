e6587e630c6a0e2ca9bb56e4120ad45b5dadfbaa
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-09-19 14:26:53 +0300

    emitWarning DEP0097

--- src/utils/emitWarning.ts
+++ src/utils/emitWarning.ts
@@ -1,12 +1,18 @@
 const originalEmitWarning: any = process.emitWarning;
 
-(process.emitWarning as any) = (warning: string | Error, type?: string, code?: string, ctor?: Function): void => {
-  if (code === 'DEP0097') {
+process.emitWarning = (...args: any[]): void => {
+  const isDEP0097 = args.filter((a) => {
+    if (typeof a === 'string' && a === 'DEP0097') {
+      return true;
+    }
+    return false;
+  }).length > 0;
+  if (isDEP0097) {
     // Undertaker uses a deprecated approach that causes NodeJS 10 to print
     // this warning to stderr:
     // "Using a domain property in MakeCallback is deprecated. Use the  async_context
     // variant of MakeCallback or the AsyncResource class instead."
     return;
   }
-  originalEmitWarning(warning, type, code, ctor);
+  originalEmitWarning(...args);
 };
