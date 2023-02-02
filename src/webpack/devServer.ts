5e90381fb336f35ae5dd12cf902796a4febaed2f
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2023-02-02 14:44:12 -0600

    Webpack Dev Server

--- src/webpack/devServer.ts
+++ src/webpack/devServer.ts
@@ -1,18 +1,19 @@
 import * as path from 'path';
 import RestProxy, { IProxySettings } from 'sp-rest-proxy/dist/RestProxy';
 import { IAppConfig, IPrivateConfig } from '../interfaces';
+import WebpackDevServer from 'webpack-dev-server';
 
-export const getDevServerOpts = (appConf: IAppConfig, privateConf: IPrivateConfig, publishPath: string) => {
+export const getDevServerOpts = (appConf: IAppConfig, privateConf: IPrivateConfig, publishPath: string): WebpackDevServer.Configuration => {
   // Webpack dev server options
   const devServerPort = parseInt(process.env.SPBUILD_WEBPACK_PORT || '9090', 10);
-  const devServerOptions = {
-    contentBase: path.join(process.cwd(), appConf.distFolder),
-    watchContentBase: true,
-    writeToDisk: true,
+  const devServerOptions: WebpackDevServer.Configuration = {
+    static: { directory: path.join(process.cwd(), appConf.distFolder) },
     // hot: true, // causes conflicts with multiple assets loading the same map files
     hot: false,
     port: devServerPort,
-    before: (app) => { // (app, server) => {
+    onBeforeSetupMiddleware: ({ app }) => {
+      if (!app) return;
+
       // Register SP API Proxy
       const settings: IProxySettings = {
         port: devServerPort,
